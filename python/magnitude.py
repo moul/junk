507b497b6d091c4aa067cfb8dfc76ac3dba02f3e
Manfred Touron <github@42.am>
2012-03-24 16:44:15 +0100

    added tests, fixed bugs, added unities

--- python/magnitude.py
+++ python/magnitude.py
@@ -2,17 +2,19 @@
 
 def parseSize(text):
     prefixes = {
-        'K': 1024,
-        'M': 1024 * 1024,
-        'G': 1024 * 1024 * 1024
+        'b': 1,
+        'k': 1024, 'ki': 1024, 'kb': 1024,
+        'm': 1024 * 1024, 'mb': 1024 * 1024,
+        'g': 1024 * 1024 * 1024, 'gb': 1024 * 1024 * 1024
         }
     num = ""
+    text = str(text).strip()
     while text and text[0:1].isdigit() or text[0:1] == '.':
         num += text[0]
         text = text[1:]
     num = float(num)
-    letter = text.strip()
+    letter = text.strip().lower()
     return num * (prefixes[letter] if letter in prefixes else 1)
 
-for test in ('1024K', '1K', '1.5M', '12M', '12', '12K'):
-    print test, parseSize(test)
+for test in ('1024K', '1.0 K', '1.5M', '12 M', '12', '12kb', '  123.421  Kb ', '1GB', 42, '42b'):
+    print test, '\r\t\t', parseSize(test)

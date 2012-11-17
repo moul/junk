e0368501866e92b571d205901f8f839af8a064b9
Manfred Touron <m@42.am>
2012-11-17 23:33:01 +0100

    added md5_file

---
+++ python/md5_file.py
@@ -0,0 +1,10 @@
+import hashlib, zlib
+
+def md5_file(f, block_size = 2 ** 20):
+    md5 = hashlib.md5()
+    while True:
+        data = f.read(block_size)
+        if not data:
+            break
+        md5.update(data)
+    return md5.hexdigest()

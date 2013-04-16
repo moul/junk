45bd89075d8cc7f2660189809c336ea8502b7a8a
Manfred Touron <m@42.am>
2013-04-16 13:23:54 +0200

    added error handling

--- python/simplecli/CliBase.py
+++ python/simplecli/CliBase.py
@@ -1,5 +1,6 @@
 import readline
 import shlex
+from sys import exit
 
 class CliBase:
     def __init__(self):
@@ -7,13 +8,21 @@ class CliBase:
 
     def run(self):
         while True:
-            raw = raw_input('prompt> ')
-            shl = shlex.shlex(raw)
-            shl.wordchars += '.-/:,='
-            shl.escapedquotes = '"\''
-            parts = list(shl)
-            if hasattr(self, 'cmd_%s' % parts[0]):
-                method = getattr(self, 'cmd_%s' % parts[0])
-                method(*parts[1:])
-            else:
-                print 'command not found'
+            try:
+                raw = raw_input('prompt> ')
+                shl = shlex.shlex(raw)
+                shl.wordchars += '.-/:,='
+                shl.escapedquotes = '"\''
+                parts = list(shl)
+                if hasattr(self, 'cmd_%s' % parts[0]):
+                    method = getattr(self, 'cmd_%s' % parts[0])
+                    method(*parts[1:])
+                else:
+                    print 'command not found'
+            except (KeyboardInterrupt,):
+                print
+            except (SystemExit):
+                print 'exit'
+            except (EOFError):
+                print 'exit'
+                exit(0)
--- python/simplecli/simplecli.py
+++ python/simplecli/simplecli.py
@@ -3,13 +3,13 @@
 import CliBase
 
 class CliPoney(CliBase.CliBase):
-    def cmd_toto(self):
-        print 'toto'
+    def cmd_toto(self, *args):
+        print 'toto', args
 
-    def cmd_titi(self):
+    def cmd_titi(self, *args):
         print 'titi'
 
-    def cmd_tata(self, arg1, arg2 = 42):
+    def cmd_tata(self, arg1, arg2 = 42, *args):
         print 'tata', arg1, arg2
 
 if __name__ == "__main__":

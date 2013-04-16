42112a4200b0587be807b239a4be11dd2a39ae2b
Manfred Touron <m@42.am>
2013-04-16 13:13:15 +0200

    added shlex support

--- python/simplecli/CliBase.py
+++ python/simplecli/CliBase.py
@@ -1,11 +1,17 @@
+import readline
+import shlex
+
 class CliBase:
     def __init__(self):
         print 'init'
 
     def run(self):
         while True:
-            cmd = raw_input('prompt> ')
-            parts = cmd.split(' ')
+            raw = raw_input('prompt> ')
+            shl = shlex.shlex(raw)
+            shl.wordchars += '.-/:,='
+            shl.escapedquotes = '"\''
+            parts = list(shl)
             if hasattr(self, 'cmd_%s' % parts[0]):
                 method = getattr(self, 'cmd_%s' % parts[0])
                 method(*parts[1:])
--- python/simplecli/simplecli.py
+++ python/simplecli/simplecli.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import readline
 import CliBase
 
 class CliPoney(CliBase.CliBase):

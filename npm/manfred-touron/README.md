f0415666c9fc9dbba639f316c5c451e4acbbf625
Manfred Touron <m@42.am>
2013-07-25 01:35:01 +0200

    initial commit

---
+++ npm/manfred-touron/README.md
@@ -0,0 +1,11 @@
+Manfred Touron
+==============
+
+```shell
+npm install manfred-touron
+```
+
+```javascript
+var mt = require('manfred-touron');
+console.log(mt.firstname);
+```
---
+++ npm/manfred-touron/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    version:   '0.0.1',
+    firstName: 'Manfred',
+    lastName:  'Touron',
+    birthDate: '01/25/1988',
+    country:   'France',
+    city:      'Paris',
+    company:   'Online OCS (Iliad)',
+    position:  'CTO',
+    alumni:    ['Epitech'],
+    networks:  ['While42', '42am']
+};

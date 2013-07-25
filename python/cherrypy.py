654dc831d1c8045f75adb620e1a3295466f18809
Manfred Touron <m@42.am>
2013-07-25 14:55:48 +0200

    cherrypy

---
+++ python/cherrypy.py
@@ -0,0 +1,15 @@
+import cherrypy
+
+class Custom(object):
+    def index(self, bub = 'default_bub'):
+        if cherrypy.request.params.get('bub', False):
+            if cherrypy.request.params['bub'] == 'bab':
+                return 'bub=bab !'
+            else:
+                return 'bad bub'
+        if cherrypy.request.login == 'bob:bib': # dont work, needs to use cherrypy.request.headers['Authorization']
+            return 'hello bob:bib'
+        return 'default'
+    index.exposed = True
+
+cherrypy.quickstart(Custom())

f96da3c43e99521e9dff9306d3e5dec8baba4b8a
Manfred Touron <m@42.am>
2013-12-20 18:43:53 +0100

    added helpers, fixed bad completion

--- js/stream-adventure/run.sh
+++ js/stream-adventure/run.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
+export NODE_PATH=$NODE_PATH:$HOME/node_modules/
+
 if [ -z $1 ]; then
     stream-adventure
     exit 0
 fi
 
-#set -x
+if [ "$1" = "all" ]; then
+    for level in *.coffee; do
+        $0 "$level"
+    done
+    exit 0
+fi
 
-export NODE_PATH=$NODE_PATH:$HOME/node_modules/
+#set -x
 
 LEVEL=$(echo $1 | sed 's/\.coffee//;s/\.js//')
 
-trap 'kill 0' SIGINT SIGTERM EXIT
+LEVEL_UPPER=$(echo "$LEVEL" | sed 's/_/ /g;y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/')
+echo $LEVEL_UPPER
 
-stream-adventure select $LEVEL >/dev/null
+stream-adventure select "$LEVEL_UPPER" >/dev/null
 
 TMPDIR=/tmp/stream-adventure-compile
 mkdir -p $TMPDIR
@@ -25,6 +33,7 @@ bash -c "$CMD1"
 bash -c "$CMD2"
 
 if [ "$2" = "watch" ]; then
+    trap 'kill 0' SIGINT SIGTERM EXIT
     fswatch . "$CMD1" &
     fswatch $TMPDIR "$CMD2"
 fi

49b4d2c7f736f405e29e7cad8015d8050a6d881e
Manfred Touron <m@42.am>
2014-01-29 19:25:43 +0100

    added wip dynamic_properties.py

--- python/dynamic_properties.py
+++ python/dynamic_properties.py
@@ -12,7 +12,11 @@ class A(object):
         if kind == 'method':
             self.__dict__[key] = child.__call__
         elif kind == 'property':
-            self.__dict__[key] = property(fget=child.__call__)
+            #setattr(self, key, property(fget=child.__call__))
+            #self.__dict__[key] = property(fget=child.__call__)
+            #setattr(self.__class__, key, property(fget=child.__call__))
+            setattr(self.__class__, key, property(fget=child.prop))
+
 
 class B(object):
     def __init__(self, name, father, key, value, kind):
@@ -22,21 +26,43 @@ class B(object):
         self.kind = kind
         print('  {}: inited (address={})'.format(name, self))
 
-    def __call__(self):
-        return self.value
+    def prop(self, *args):
+        return self.__call__()
+
+    def __call__(self, *args):
+        return [self.value] + list(args)
+
+
+def print_a(a):
+    print('-' * 80)
+    print(a.name)
+    print('  dir:  {}'.format([key for key in dir(a) if key[0] != '_']))
+    print('  dict: {}'.format(a.__dict__))
+    for key in ['key42', 'key43', 'key44', 'key45']:
+        try:
+            print('    {}={}'.format(key, getattr(a, key)))
+            try:
+                print('    {}()={}'.format(key, getattr(a, key)()))
+            except:
+                print('    {}() raised'.format(key))
+        except:
+            print('    {} raised'.format(key))
+        print('')
+    print('-' * 80)
+    print('\n' * 2)
+
 
 def main():
     a = A('George')
     a.register('Bobby', B, 'key42', 42, 'method')
     a.register('Tom', B, 'key43', 43, 'property')
+    print_a(a)
 
-    print('-' * 80)
-    print(dir(a))
-    print(a.__dict__)
-    print(a.key42)
-    print(a.key42())
-    print(a.key43)
-    print('-' * 80)
+    c = A('Alain')
+    c.register('Tiffany', B, 'key44', 44, 'method')
+    c.register('Barney', B, 'key45', 45, 'property')
+    print_a(c)
+
 
 if __name__ == "__main__":
     main()
